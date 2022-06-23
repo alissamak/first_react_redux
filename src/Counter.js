da09005f5490e1c0933bf150489c3e4b2abe932e
Alissa <alissamak@outlook.com>
2022-06-22 22:00:46 -0400

    completed activity with react-redux counter

---
+++ src/Counter.js
@@ -0,0 +1,31 @@
+import React, {useState} from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { increment, decrement, incrementByAmount } from './features/counterSlice';
+
+const Counter = () => {
+    //calling useSelector to grab current value of stat variable from the store
+    const count = useSelector((state) => state.value);
+    //dispatch hook
+    const dispatch = useDispatch();
+    const [input, setInput] = useState(0);
+    
+    const byAmountSubmit = (e) => {
+        e.preventDefault()
+        dispatch(incrementByAmount(Number(input)))
+    }
+    
+    return (
+        <div>
+            <h1>
+                {count}
+            </h1>
+            <button onClick={() => dispatch(increment())}>Increment</button>
+            <button onClick={() => dispatch(decrement())}>Decrement</button>
+            <form onSubmit={(e) => byAmountSubmit(e)}>
+                <input type="number" onChange={(e) => setInput(e.target.value)}></input>
+                <button type="submit">Submit</button>
+            </form>
+        </div>
+    )
+}
+export default Counter;
